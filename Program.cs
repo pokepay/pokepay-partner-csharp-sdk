9d7683a8ee543652e0cd56e24394381518c007c2
smihica <smihica@gmail.com>
2021-09-30 06:27:57 +0900

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace pokepay_partner_csharp_sdk
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new Client("config.ini");
+            var sendEchoRequest = new SendEcho("message1");
+
+            var echo = await sendEchoRequest.Send(client);
+
+            Console.WriteLine("{0}", JsonSerializer.Serialize(echo));
+        }
+    }
+}
