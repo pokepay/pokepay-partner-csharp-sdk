b4b5c5971e9fbd0c6fc6f2cad10b54936a5e1ed6
SASAO Takahiro <takahiro_sasao@pocket-change.jp>
2022-05-24 13:44:13 +0900

    add build configurations for .net framework 4.8 (windows only)

---
+++ clean.cmd
@@ -0,0 +1,11 @@
+cd src\PokepayPartnerCsharpSdk
+rmdir /s /q bin obj
+cd ..\..
+
+cd src\PokepayPartnerCsharpSdk.Example
+rmdir /s /q bin obj
+cd ..\..
+
+cd src\PokepayPartnerCsharpSdk.Test
+rmdir /s /q bin obj
+cd ..\..
